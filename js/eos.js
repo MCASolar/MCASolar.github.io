1b86cb5d60044a90d0a9079ce619c29ae5c36636
Jabez Wesly <wjabezkingsly@gmail.com>
2015-12-20 22:27:05 -0500

    Mission Statement OG

--- js/eos.js
+++ js/eos.js
@@ -1,16 +1,30 @@
-var scene = new THREE.Scene();
+/*var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight * .92 );
 document.getElementById("home").appendChild( renderer.domElement );
 
-			var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-			var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+			var material = new THREE.MeshBasicMaterial( { color: 0x00ffff } );
+
+var loader = new THREE.STLLoader();
+loader.load("res/car_sample.stl", function(geo){
+	var car = new THREE.Mesh(geo);
+	car.rotation.z = Math.PI/4;
+	car.rotation.x = Math.PI/4;
+	car.rotation.y = Math.PI/4;
+	scene.add(car);
+})
+
+			
 			var cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
+			//scene.add( cube );
 
 			camera.position.z = 5;
+
+			var controls = new THREE.OrbitControls(camera);
 
 			var render = function () {
 				requestAnimationFrame( render );
@@ -22,3 +36,4 @@ document.getElementById("home").appendChild( renderer.domElement );
 			};
 
 			render();
+*/
